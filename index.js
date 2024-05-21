8af1aa768974f0fd09db9d3176baaa040a8e5a6c
Moraj <109418239+morajdhwaj@users.noreply.github.com>
2024-05-21 16:22:03 +0530

    [INIT] API

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 import inquirer from "inquirer";
 import readline from "readline";
-
-// displayAsciiArt.js
+import axios from "axios";
+import fs from "fs"; // Step 1: Import the fs module
 
 // Step 1: Define ANSI escape codes for colors
 const colors = {
@@ -34,29 +34,39 @@ Enter to continue to integrate Consent Collection in our application `;
 
 console.log(asciiArt);
 console.log(decs);
-
 const questions = [
   {
     type: "input",
-    name: "ProjectName",
+    name: "projectName",
     message: "What is your project name?",
+    validate: (input) => {
+      return input !== "" ? true : "Project name is required";
+    },
   },
   {
     type: "input",
-    name: "Email",
-    message: "What is your  email?",
+    name: "email",
+    message: "What is your email?",
+    validate: (input) => {
+      return input !== "" ? true : "Email is required";
+    },
   },
   {
     type: "input",
-    name: "Mobile ",
+    name: "mobile",
     message: "What is your mobile number?",
+    validate: (input) => {
+      return input !== "" ? true : "Mobile number is required";
+    },
   },
-
   {
     type: "list",
     name: "Customers",
     message: "Number of tentative customers for consent collection:",
     choices: ["1-1k", "1k-100k", "100k-500k", "500k+"],
+    validate: (input) => {
+      return input !== "" ? true : "Please select number of customers";
+    },
   },
 ];
 
@@ -96,9 +106,56 @@ const waitForEnter = async () => {
   });
 };
 
+const sendData = async (data) => {
+  const options = {
+    method: "POST",
+    url: "https://npm-info-user-backend.onrender.com/user-form",
+    headers: { "Content-Type": "application/json" },
+    data: data,
+  };
+
+  try {
+    const response = await axios.request(options);
+    console.log("Response from API:", response.data);
+
+    // Step 3: Save response to .env file
+    saveToEnv(response.data);
+  } catch (error) {
+    console.error("Error from API:", error);
+  }
+};
+
+// Step 2: Create function to write to .env file
+const saveToEnv = (data) => {
+  const envFilePath = ".env";
+  let envContent = "";
+
+  // Prepare content to write
+  for (const key in data) {
+    envContent += `${key.toUpperCase()}=${data[key]}\n`;
+  }
+
+  // Write or append to .env file
+  fs.appendFile(envFilePath, envContent, (err) => {
+    if (err) {
+      console.error("Error writing to .env file:", err);
+    } else {
+      console.log(".env file updated successfully.");
+    }
+  });
+};
+
 (async () => {
   await waitForEnter();
   const answers = await askQuestions();
   console.log("\nAll answers received:");
   console.log(answers);
+
+  // Send data to API
+  await sendData({
+    project_name: answers.projectName,
+    email: answers.email,
+    mobile: answers.mobile,
+    number_of_customers: answers.Customers,
+  });
 })();

12fca624f1951b9cd1bc2a2aa5281376c0e6c912
Moraj <109418239+morajdhwaj@users.noreply.github.com>
2024-05-22 13:04:02 +0530

    [UPDATE] create a  individual file title

---
+++ Title.js
@@ -0,0 +1,28 @@
+// colors.js
+
+export const colors = {
+  reset: "\x1b[0m",
+  red: "\x1b[31m",
+  green: "\x1b[32m",
+  yellow: "\x1b[33m",
+  blue: "\x1b[34m",
+  magenta: "\x1b[35m",
+  cyan: "\x1b[36m",
+  white: "\x1b[37m",
+};
+
+export const asciiArt = ` \x1b[33m
+  
+  ██████╗ ██████╗ ███╗   ██╗ ██████╗██╗   ██╗██████╗     
+  ██╔════╝██╔═══██╗████╗  ██║██╔════╝██║   ██║██╔══██╗    
+  ██║     ██║   ██║██╔██╗ ██║██║     ██║   ██║██████╔╝    
+  ██║     ██║   ██║██║╚██╗██║██║     ██║   ██║██╔══██╗    
+  ╚██████╗╚██████╔╝██║ ╚████║╚██████╗╚██████╔╝██║  ██║    
+   ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝  \x1b[0m
+  `;
+
+export const decs = `Concur - Consent Manager is open source consent manager that 
+helps companies comply with Digital Personal Data Protection Act, 
+2023. Some more content will come here for ready to read and get
+education about concur To Know more Visit: https://concur.live
+integrate Consent Collection in our application `;
--- index.js
+++ index.js
@@ -3,36 +3,8 @@
 import inquirer from "inquirer";
 import readline from "readline";
 import axios from "axios";
+import { asciiArt, decs } from "./Title.js";
 import fs from "fs"; // Step 1: Import the fs module
-
-// Step 1: Define ANSI escape codes for colors
-const colors = {
-  reset: "\x1b[0m",
-  red: "\x1b[31m",
-  green: "\x1b[32m",
-  yellow: "\x1b[33m",
-  blue: "\x1b[34m",
-  magenta: "\x1b[35m",
-  cyan: "\x1b[36m",
-  white: "\x1b[37m",
-};
-
-// Step 2: Define the ASCII art with color
-const asciiArt = ` \x1b[33m
-
-██████╗ ██████╗ ███╗   ██╗ ██████╗██╗   ██╗██████╗     
-██╔════╝██╔═══██╗████╗  ██║██╔════╝██║   ██║██╔══██╗    
-██║     ██║   ██║██╔██╗ ██║██║     ██║   ██║██████╔╝    
-██║     ██║   ██║██║╚██╗██║██║     ██║   ██║██╔══██╗    
-╚██████╗╚██████╔╝██║ ╚████║╚██████╗╚██████╔╝██║  ██║    
- ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝  \x1b[0m
-`;
-
-const decs = `Concur - Consent Manager is open source consent manager that 
-helps companies comply with Digital Personal Data Protection Act, 
-2023. Some more content will come here for ready to read and get
-education about concur To Know more Visit: https://concur.live
-Enter to continue to integrate Consent Collection in our application `;
 
 console.log(asciiArt);
 console.log(decs);
